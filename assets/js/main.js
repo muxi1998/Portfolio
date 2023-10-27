a69520496ef6407aa8c9c260bea8a5126f543682
Chen Mu-Hsi <louis19980927@gmail.com>
2023-10-27 18:42:34 +0800

    [Add] Create the event of opening/closing the menu container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu') // add class show-menu to navMenu, so it will show the menu
+    })
+}
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if (navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu') // remove class show-menu to navMenu, so it will hide the menu
+    })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
